1c159a891fd28b5aba54c615ec9e30fab17df2dc
George Chaves <george_hfc@yahoo.com.br>
2023-02-03 12:13:25 +0000

    adicionada prática da faculdade

--- 01_hello_py/hello.py
+++ 01_hello_py/hello.py
@@ -4,3 +4,13 @@ first_name = input('Type your name: ')
 last_name = input('Type your surname: ')
 name = first_name + " " + last_name
 print("{}".format(name) + " have a great day! ;)")
+
+# TODA EXPRESSÃO TEM UM VALOR
+print(type(1))
+print(type(2))
+divisao = 1 / 2             # FLOAT
+print(type(divisao))
+print(divisao)
+divisao_inteira = 7 // 2    # 3
+print(divisao_inteira)
+print('-' * 5)
---
+++ 02_conditionals/horas_ano.py
@@ -0,0 +1,12 @@
+# ATÉ A VIRADA DO SÉC. PASSADO (XIX–XX) AS PESSOAS TRABALHAVAM EM MÉDIA
+# 3200 horas/ano. COM A REVOLUÇÃO INDUSTRIAL (TEMPOS ATUAIS) AS PESSOAS
+# TRABALHAM EM MÉDIA 1600 horas/ano. AGORA QUE TEM TEMPO ESSAS HORAS
+# SÃO GASTAS BASICAMENTE EM SAÚDE / EDUCAÇÃO / LAZER
+# – – – – – – – – – – – – – – – –
+# MAS QUANTAS HÁ EM UM ANO?
+print("Quantas horas há em um ano?")
+print("1 dia = 24 horas")
+print("1 ano = 365 dias = 365 * 24 horas")
+horas_ano = 365 * 24
+print("1 ano = {} horas".format(horas_ano))
+print("###### AGORA VOCÊ ######")
