49a7b827d7cf5a42af343d27bc818835fd09f63a
George Chaves <george_hfc@yahoo.com.br>
2023-02-09 15:44:08 +0000

    coding: utf-8

--- 01_hello_py/electric_scooter.py
+++ 01_hello_py/electric_scooter.py
@@ -1,7 +1,8 @@
+# coding: utf-8
 # SCRIPT TO CALCULATE AVG SPEED OF SCOOTERS IN SÃO PAULO
 print("### 🛴 ELECTRIC SCOOTER 🛴 ###")
 print("This script calculates the average speed of a scooter")
 dist = input("Type the travelled distance (kilometers): ")
-tempo = input("Quantos minutos o patinete demorou para percorrer essa distância? ")
-velocidade_media = float(distancia) / float(tempo)
-print("O patinete atingiu uma velocidade de {} m/min".format(velocidade_media))
+time = input("How long did it take the scooter to stop (minutes)? ")
+avg_speed = float(dist) / float(time)
+print("The scooter reached {} m/min".format(avg_speed))
---
+++ 02_variables_decision_making/lec.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+# PRACTICE USING LECTURE MATERIAL
+ano = 1989
+nome = "Luke Skywalker"
+saldo = 50
+print("Ano: {}, Nome: {}, Saldo: R${:.1f}".format(ano, nome, saldo))
+print("O tipo da variável ano é {}".format(type(ano)))
+print("O tipo da variável nome é {}".format(type(nome)))
+print("O tipo da variável saldo é {}".format(type(saldo)))
