a1f4d7c90ee5e100226395822f4feeeebd33a907
George Chaves <george_hfc@yahoo.com.br>
2023-02-10 12:07:22 +0000

    second class exercises

--- 02_variables_decision_making/lec.py
+++ 02_variables_decision_making/lec.py
@@ -7,3 +7,28 @@ print("Ano: {}, Nome: {}, Saldo: R${:.1f}".format(ano, nome, saldo))
 print("O tipo da variável ano é {}".format(type(ano)))
 print("O tipo da variável nome é {}".format(type(nome)))
 print("O tipo da variável saldo é {}".format(type(saldo)))
+
+# 09FEV • AULA02
+# NÃO EXISTE MATRIZ NO PYTHON
+# porque não existe tamanho fixo na linguagem
+# https://numpy.org/
+# interpolação (format, f-string, %)
+
+# SLICE <~~~
+nomes = [ 'a', 'b', 'c', 'd' ]
+print(nomes[ 1: ]) 				# ‘b’, ‘c’, ‘d’
+print(nomes[ 1:-1 ]) 				# ‘b’, ‘c’
+nomes.append('e')		# ‘a’, ‘b’, ‘c’, ‘d’, ‘e’
+
+import time
+# FOR <~~~
+for item in nomes:
+    time.sleep(1)
+    print(item)
+
+# RANGE <~~~
+for number in range(10):
+    time.sleep(0.5)
+    print(number)
+
+# WHILE <~~~
---
+++ 02_variables_decision_making/letter.py
@@ -0,0 +1,37 @@
+# SCRIPT THAT WON'T DISMISS UNTIL LETTER IS TYPED
+# PRACTICING THE WHILE
+# MADE IN CLASS 10Fev
+print("### 🔤 FIND THE LETTER 🔤 ###")
+letter = input("Input a letter: ")
+
+while letter != 'x':
+    print("Wrong letter...")
+    letter = input("Type another letter: ")
+
+print("Finally! You found '{}'.".format(letter))
+
+# while True:
+#     letter = input("Input a letter: ")
+#     if letter == 'x':
+#         break
+#     print("Wrong letter...")
+
+# DICTIONARY (TUPLA)
+print("##########")
+sheet = ('George', 33, 'Lisbon', 'Portugal')
+print(f'Name: {sheet[0]}')
+print(f'Age: {sheet[1]}')
+sheet = [{
+    "Name": "George",
+    "Surname": "Chaves",
+    "test"
+    "Age": 33,
+    "City": "Lisbon",
+    "Country": "Portugal"
+}]
+# print(sheet[0]["Name"])
+for person in sheet:
+    for key, value in person.items():           # .items() IS BUILT-IN
+            print(f"{key}   •   {value}")
+    print("*" * 10)
+print(type(person))
