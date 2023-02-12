57880bf1938ba6a04d209bbde5533ae961869198
George Chaves <george_hfc@yahoo.com.br>
2023-02-12 11:23:42 +0000

    for loops

---
+++ 03_repetition/for.py
@@ -0,0 +1,25 @@
+# PRACTICING FOR LOOPS
+print("### 4️⃣ LOOP FOR 🎢 ###")
+# VERY SIMPLE RANGE
+for x in range(10):
+    print(x)
+
+print("#####")
+# RANGE BEGINNING WITH...
+for y in range(2, 10):
+    print(y)
+
+print("#####")
+# RANGE WITH INCREMENTS
+for z in range(10, 101, 20):
+    print(z)
+
+print("#####")
+# NOW EVEN NUMBERS FROM 2 TO 1000
+for x in range(2, 1001, 2):
+    print(x)
+
+print("#####")
+# ODD NUMBERS FROM 1 TO 1000
+for x in range(1, 1001, 2):
+    print(x)
