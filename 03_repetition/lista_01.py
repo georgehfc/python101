bb1fde2d4a5c9f1d5e9e9411f8c6177988486931
George Chaves <george_hfc@yahoo.com.br>
2023-02-13 02:17:42 +0000

    add lista01

---
+++ 03_repetition/lista_01.py
@@ -0,0 +1,8 @@
+# 1. Faça um programa que mostre a frase "Estou aprendendo Python!"
+print("Estou aprendendo Python!")
+# 2. Faça um programa que peça o nome da pessoa e mostre a frase "(pessoa) está
+# aprendendo Python!", onde o (pessoa) tem que ser substituído pelo nome da pessoa.
+name = input("Nome: ")
+print("{} está aprendendo Python!".format(name))
+# 3. Faça um programa que peça 2 números e mostre o resultado das 4 operacoes entre os dois
+# números: soma, subtração, divisão e multiplicação.
